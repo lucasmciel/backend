6751e91aebd00c4b4dcea0e03425eddcc70b44a3
Lucas Maciel <lucas.maciel.lm@hotmail.com>
2018-07-20 14:40:53 -0300

    melhorias

--- cadastroweb/cadastro.php
+++ cadastroweb/cadastro.php
@@ -2,16 +2,70 @@
 <html>
 <head>
 	<title>Cadastro na Tabela</title>
+	<style type="text/css">
+		*{
+			margin: 0;
+			padding: 0;
+		}
+
+		body{
+			font-family: Verdana;
+			background-color: #ddd;
+			font-size: 100%;
+		}
+
+
+		h1{
+			padding-left: 580px;
+		}
+
+		section{
+			padding: 17px;
+			padding-left: 500px;
+			padding-top: 10px;
+			box-sizing: border-box;
+		}
+
+		.campo{
+			width: 350px;
+			height: 25px;
+			margin-top: 3px;
+			margin-bottom: 3px;
+
+		}
+
+		.btn{
+			width: 100px;
+			height: 30px;
+			cursor: pointer;
+			font-family: Verdana;
+			margin-left: 120px;
+			margin-top: 3px;
+
+		}
+
+	</style>
 </head>
 <body>
-	<h2>Cadastre-se</h2>
-	<form method="get" action="grava.php" class="formulario"> 
-		<label>Nome: </label><input type="text" name="nome" size="30"><br>
-		<label>Telefone: </label><input type="text" name="telefone" size="30"><br>
-		<label>Email: </label><input type="text" name="email" size="30"><br>
-		<label>Id: </label><input type="text" name="tipo_id" size="30"><br>
-		<input type="submit" name="enviar" value="Enviar">
-	</form>
-
+	<div class="container">
+		<nav class="cadastro">
+	
+			<h1>Cadastre-se</h1>
+			<hr>
+		</nav>
+			<section>
+				<form method="get" action="grava.php" class="formulario"> 
+				Nome: <br>
+				<input type="text" name="nome" class="campo" required autofocus><br>
+				Telefone: <br>
+				<input type="text" name="telefone" class="campo"><br>
+				Email: <br>
+				<input type="text" name="email" class="campo"><br>
+				Id: <br>
+				<input type="text" name="tipo_id" class="campo"><br>
+				<input type="submit" name="enviar" value="Cadastrar" class="btn">
+				</form>
+			</section>	
+	</div>
 </body>
 </html>
